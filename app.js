60bd1a66b1ebf1be4e0fa81ee2fe44342e3ef7dc
Roman Sadoyan <sadoyan@protonmail.com>
2020-04-30 18:47:17 +0300

    prettify

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 
-const needle = require('needle');
+const needle = require("needle");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   if (!req.query.url) {
     return res.status(400).json({
       success: false,
-      message: 'Url not provided'
+      message: "Url not provided",
     });
   }
-  needle.get(req.query.url, function(error, response) {
-    if (!error && response.statusCode === 200){
+  needle.get(req.query.url, function (error, response) {
+    if (!error && response.statusCode === 200) {
       res.send(response.body);
-    }else{
+    } else {
       res.status(500).json({
         success: false,
-        message: 'Remote server error'
+        message: "Remote server error",
       });
     }
   });
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+app.use(function (req, res, next) {
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   let response = {
     success: false,
     code: err.status || 500,
-    message: err.message
+    message: err.message,
   };
   response =
-    req.app.get('env') !== 'production' ?
-      Object.assign(response, {
-        stack: err.stack
-      }) :
-      response;
+    req.app.get("env") !== "production"
+      ? Object.assign(response, {
+          stack: err.stack,
+        })
+      : response;
 
   res.status(err.status || 500);
   res.json(response);
